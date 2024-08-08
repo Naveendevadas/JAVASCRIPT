ebdba6b69e6e9c70fce39bc4b8571dddc78da21f
naveen <naveenmd003@gmail.com>
2024-08-08 09:37:48 +0530

    work

--- javas.js
+++ javas.js
@@ -1,57 +1,50 @@
 
 
 {
-    const people = [
+    let people = [
 
-    {name : "Alice",age :30,city : "New york"},
-    {name : "Bob",age :25,city : "los angels"},
-    {name : "Alice",age :30,city : "New york"},
-    {name : "Alice",age :30,city : "New york"},
-    {name : "Alice",age :30,city : "New york"}
-   
-];
-console.log("people :", people);
+        { name: "Alice", age: 30, city: "New york" },
+        { name: "Bob", age: 25, city: "los angels" },
+        { name: "Alice", age: 30, city: "New york" },
+        { name: "Alice", age: 30, city: "New york" },
+        { name: "Alice", age: 30, city: "New york" }
+
+    ];
+    console.log("people :", people);
+
+    console.log("\n\n\n\n\n\n\n\n\n\n\n")
+
+
+    let result = people.forEach((item) => {
+        console.log("item :",item);
+        
+    
+    })
+
 }
 
+console.log("\n\n\n")
 
-// {
-
-// // task map
-// let arr [10,20,30];
-
-// let result = arr.map ((item) => {
-//     return item + 2;
-// });
-// console,log("result2 ;" , result2);
-
-
-
-// }
-
-
-
-
-// constructor
+constructor
 
 {
     // using funtion
 
 
-    function Person(name,age,city){
+    function Person(name, age, city) {
         this.name = name;
         this.age = age;
         this.mark = city;
-    
+
         this.greeting = function () {
             console.log(`hai ${this.name}, your mar ${this.mark}`)
         }
     }
-    let person1 = new Person('naveeen',20,20);
-    console.log("person1:",person1);
+    let person1 = new Person('naveeen', 20, 20);
+    console.log("person1:", person1);
     person1.greeting();
-    
 
-}{
+} {
 
     // using class
 
@@ -62,7 +55,7 @@ console.log("people :", people);
         age;
         mark;
 
-        constructor(name,age,mark) {
+        constructor(name, age, mark) {
             this.name = name;
             this.age = age;
             this.mark = mark;
@@ -70,14 +63,14 @@ console.log("people :", people);
 
         greeting() {
             console.log(`Hello ${this.name}, your age is ${this.age}, and your Mark is ${this.mark}`);
-        
+
         }
     }
 
-        let person1 = new Person("naveen",21,100);
-        console.log("person1 :",person1);
-        person1.greeting();
-    
+    let person1 = new Person("naveen", 21, 100);
+    console.log("person1 :", person1);
+    person1.greeting();
+
 }
 
 //
@@ -108,79 +101,86 @@ console.log("\n\n\n")
 //     animal1.greeting();
 // }
 
+// {
+//     class Animal {
 
-//
+//         name;
 
+//         constructor(name) {
+//             this.name = name;
+//         }
+
+//         walking() {
+//             console.log(`${this.name} is a dog`);
+//         }
+//     }
+
+//     let animal2 = new Animal("max");
+//     console.log("animal2 :", animal2);
+//     animal2.walking();
+
+
+//     class Cat extends Animal {
+
+//         color;
+
+//         constructor(name, color) {
+//             super(name)
+//             this.color = color;
+//         }
+
+//         walking() {
+//             console.log(`${this.name} is a ${this.color} cat`)
+//         }
+
+//     }
+
+//     let cat1 = new Cat("moc", "green");
+//     console.log("cat1 :", cat1);
+//     cat1.walking();
+// }
+
+// console.log("\n\n")
+
+// {
+//     class Car {
+
+//         name;
+
+//         constructor(name) {
+//             this.name = name;
+//         }
+
+//         info() {
+//             console.log(`${this.color} color car is a ${this.name}`)
+//         }
+
+//         set Color(color) {
+//             this.color = color;
+
+//         }
+
+
+//         get Color() {
+//             return this.Color;
+//         }
+
+//     }
+
+//     let car1 = new Car("Audi rs6");
+//     car1.color = "red";
+
+//     console.log("car1 :", car1);
+//     car1.info();
+
+//     console.log("car1 :", car1.color);
+
+
+// }
+
+console.log("\n\n\n")
 
 {
-    class Animal{
-
-        name ;
-
-        constructor(name) {
-            this.name = name ;
-        }
-
-        walking() {
-            console.log(`${this.name} is a dog`);
-        }
-    }
-    
-    let animal2 = new Animal("max") ;
-    console.log("animal2 :",animal2);
-    animal2.walking();
-
-
-    class Cat extends Animal {
-
-        color;
-
-        constructor(name,color){
-            super(name)
-            this.color = color;
-        }
-
-        walking(){
-            console.log(`${this.name} is a ${this.color} cat`)
-        }
-
-    }
-
-    let cat1 =new Cat("moc","green");
-    console.log("cat1 :",cat1);
-    cat1.walking();
 }
 
-console.log("\n\n")
 
-{
-    class Car{
-
-        name;
-
-        constructor(name){
-            this.name = name;
-        }
-
-        info(){
-            console.log(`${this.color} color car is a ${this.name}`)
-        }
-
-        set Color(color) {
-            this.color = color ;
-          
-        }
-        get color(){
-            return this.color;
-
-        }
-
-    }
-
-    let car1 = new Car("Audi rs6");
-    car1.color = "red";
-    console.log("car1 :", car1)
-    car1.info();
-    console.log("color:",color)
-
-}
