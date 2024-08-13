5a8efeab082ed4fbbef4744fab6f85afc273f0b4
naveen <naveenmd003@gmail.com>
2024-08-14 00:09:43 +0530

    htth

---
+++ script.js
@@ -0,0 +1,25 @@
+let xhr = new XMLHttpRequest()
+console.log("req :",xhr);
+xhr.open('get',);
+xhr.send();
+
+xhr.onreadystatechange = function () {
+    console.log("readystate :", xhr.readyState);
+    if (xhr.readyState === 4) {
+        console.log("status :",xhr.status);
+    }
+    if (xhr.status === 200) {
+        let response = xhr.response;
+        console.log("response :", response);
+        let data =JSON.parse(response);
+        console.log(data);
+        let texterss = document.getElementsByClassName("texterss");
+        // let texterss = document.getElementsByClassName("texterss");
+        // let texterss = document.getElementsByClassName("texterss");
+        // let texterss = document.getElementsByClassName("texterss");
+
+        for (let i=0; i<texterss.length && i<data.length; i++) {
+            texterss[i].innerHTML = (data[i].texterss).slice(0,20)+ "..."
+        }
+    }
+}
